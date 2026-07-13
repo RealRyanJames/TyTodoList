4fe06dc968d6be9b8227cef6ab1cd11822c76a72
RealRyanJames <realcoder2023@gmail.com>
2026-07-13 13:23:33 -0400

    added mongodb code connect and insert data to documents to db

--- Main.ts
+++ Main.ts
@@ -1,12 +1,38 @@
 import readline from "readline-sync";
 import fs from "fs";
+import { MongoClient } from "mongodb";
 
 const getTodos = (a: string[]) => {
   return a.reduce((index, e) => index + e);
 };
 
+interface DataDB {
+  getDBContext(data: string): void;
+}
+
 interface IDataTodos {
   getDate(): void;
+}
+
+class DataDBContext implements DataDB {
+  toLower(uri: string): string {
+    return uri.toLowerCase();
+  }
+
+  getURL(): string {
+    return this.toLower("www.mongodb://localhost:27017");
+  }
+
+  async getDBContext(data: string): Promise<void> {
+    const dbClient = new MongoClient(this.getURL());
+    const db = dbClient.db("todos");
+    const coll = db.collection("todo");
+    await coll.insertOne({ "Todos:": data });
+  }
+
+  async setup(data: string): Promise<void> {
+    await this.getDBContext(data);
+  }
 }
 
 class Dates implements IDataTodos {
@@ -19,7 +45,7 @@ class Dates implements IDataTodos {
 const date = new Dates();
 date.getDate();
 
-function main() {
+async function main() {
   const todos: string[] = [];
   const todo_1 = readline.question("Enter Todo 1: ");
   const todo_2 = readline.question("Enter Todo 2: ");
@@ -33,6 +59,8 @@ function main() {
 
   for (const s in todos) {
     console.log(getTodos(todos));
+    const newDB = new DataDBContext();
+    await newDB.setup(getTodos(todos));
   }
 
   setTimeout(() => {
@@ -43,11 +71,4 @@ function main() {
   }, 3000);
 }
 
-class CallableFunc {
-  getMain() {
-    main();
-  }
-}
-
-let c = new CallableFunc();
-c.getMain();
+main();
